51d1612925bb9836ba3eeff3c8915fa4f1c5ba7c
onix-xcc <onix.xcc@gmail.com>
2019-05-06 00:29:54 -0500

    Adjusted connection.js code because I just know i was going to have a bug later on, created orm.js, set up the dependency on the connection.js, put in the name of the three methods and left them empty and put the export at the end, gonna take off for the night.

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,25 @@ var mysql = require("mysql");
 // Shortcut variables to save me some typing
 var cl = console.log;
 
-
-var dbConnection = mysql.createConnection({
+// Connection credentials to MySQL database. 
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
+
+    // Don't forget to put in the password later to test app out!
     password: "",
+
     database: "burgers_db"
 });
 
-dbConnection.connect(function(err) {
+// Function that connects Node to MySQL db
+connection.connect(function(err) {
     if (err) {
         cl("Connection error: " + err.stack);
         return;
     }
-    cl("Connected as id " + dbConnection.threadId);
+    cl("Connected as id " + connection.threadId);
 });
 
-module.exports = dbConnection;
+// Exports Node to MySQL connection
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// Dependencies
+var dbConnection = require("./connection");
+
+// Shortcut variables to save me some typing
+var cl = console.log;
+
+selectAll()
+
+insertOne()
+
+updateOne()
+
+
+module.exports = orm;
