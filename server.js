3b433294ff79befadef879ab1e7bb6b5bf778d33
Castro Camacho <OCASTROCAMACHO@dallasisd.org>
2019-05-09 19:53:34 -0500

    Worked on the server.js from work laptop, commiting differences to keep working on my main pc.

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 // Dependencies
 var express = require("express");
 
-
-
-
-
 // Set a dynamic port and a local port at 6060
 var PORT = process.env.PORT || 6060;
 
+var burgerApp= express();
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+burgerApp.use(express.static(__dirname + "/public"));
+
+// Parse application body as JSON
+burgerApp.use(express.urlencoded({ extended: true }));
+burgerApp.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+burgerApp.engine("handlebars", exphbs({ defaultLayout: "main" }));
+burgerApp.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+burgerApp.use(routes);
+
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+burgerApp.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("App server listening on: http://localhost:" + PORT);
   });
